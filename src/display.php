c038a8022642eeff7fb4976c0f54ff756dd60568
Elena Ionescu <ionesew6@mc-531158.immediate.media>
2017-03-08 11:12:43 +0000

    small change

--- src/display.php
+++ src/display.php
@@ -2,6 +2,7 @@
 require_once('programmes.php');
 $keyword = $_POST['search'];
 
+
 if (!empty($keyword)) {
     $myProgrammes = new Programmes();
     $programmes = $myProgrammes->getProgrammes(array('search' => $keyword));
